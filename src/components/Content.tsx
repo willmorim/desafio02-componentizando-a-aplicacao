0db69c839bd1e698ce2969e03d1e729779189468
willmorim <pdbawilliam@gmail.com>
2021-03-21 01:14:27 -0300

    Complete challenge 02 componentizando a aplicação

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+interface MovieProps {
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+  selectedGenre: {
+    title: string;
+  }
+}
+
+export function Content({ movies, selectedGenre }: MovieProps ) {
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} 
+              title={movie.Title} 
+              poster={movie.Poster} 
+              runtime={movie.Runtime} 
+              rating={movie.Ratings[0].Value} 
+            />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  genres: Array<{
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  }>;
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: GenreResponseProps) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+      </nav>
+  )
 }
